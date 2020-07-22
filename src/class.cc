6f7a203f8695f8f3a041b175501df24da3f32bcf
Duong Dac <ddac@google.com>
2020-07-22 14:06:04 +0200

    Init GenerateAnimation.

--- src/class.cc
+++ src/class.cc
@@ -17,7 +17,10 @@
 namespace libwebp {
 
 Thumbnailer::Thumbnailer() {}
-Thumbnailer::~Thumbnailer() {}
+Thumbnailer::~Thumbnailer() {
+  WebPAnimEncoderDelete(enc);
+  WebPDataClear(&webp_data);
+}
 
 // Let's implement stuff !
 // TODO: to create an animation:
@@ -28,6 +31,20 @@ bool Thumbnailer::AddFrame(const WebPPicture& pic, int timestamp_ms) {}
 //       - call WebPAnimEncoderNew, then fill the animation with WebPPicture
 //         using WebPAnimEncoderAdd
 //       - finalize with WebPAnimEncoderAssemble
-bool Thumbnailer::GenerateAnimation(WebPPicture* const output) {}
+bool Thumbnailer::GenerateAnimation(WebPPicture* const output) {
+  // Add last frame
+  WebPAnimEncoderAdd(enc, NULL, frames.back().timestamp_ms, NULL);
+  WebPAnimEncoderAssemble(enc, &webp_data);
+
+  // Set loop count
+  WebPMuxAnimParams new_params;
+  WebPMux* const mux = WebPMuxCreate(&webp_data, 1);
+  new_params.loop_count = loop_count;
+  WebPMuxSetAnimationParams(mux, &new_params);
+
+  WebPDataClear(&webp_data);
+  WebPMuxAssemble(mux, &webp_data);
+  WebPMuxDelete(mux);
+}
 
 }  // namespace libwebp
