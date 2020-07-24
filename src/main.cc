2d27847c2fe462bf9aac2e4b4c84aedcb493e821
Duong Dac <ddac@google.com>
2020-07-24 15:27:41 +0200

    Init main.cc.

--- src/main.cc
+++ src/main.cc
@@ -12,14 +12,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "class.h"
 
-int main(int argc, char *argv[]) {
+// Return true on success or false on failure.
+bool ReadImage(const char filename[], WebPPicture* const pic) {
+  const uint8_t* data = NULL;
+  size_t data_size = 0;
+  WebPImageReader reader;
+  if (!ImgIoUtilReadFile(filename, &data, &data_size)) goto out;
+  reader = WebPGuessImageReader(data, data_size);
+  if (!reader(data, data_size, pic, 1, NULL)) goto out;
+  return true;
+out:
+  free((void*)data);
+  return false;
+}
+
+int main(int argc, char* argv[]) {
   // TODO: read images given as arguments
   // TODO: create an animation
   // TODO: write the animation to disk
+
+  libwebp::Thumbnailer thumbnailer = libwebp::Thumbnailer();
+  WebPPicture pic;
+  WebPData webp_data;
+
+  WebPDataInit(&webp_data);
+  WebPPictureInit(&pic);
+
+  std::ifstream input_list(argv[1]);
+  const char* output = argv[2];
+  std::string filename_str;
+  int timestamp_ms;
+
+  while (input_list >> filename_str >> timestamp_ms) {
+    char filename[filename_str.length()];
+    strcpy(filename, filename_str.c_str());
+    std::cout << ReadImage(filename, &pic);
+    thumbnailer.AddFrame(pic, timestamp_ms);
+  }
+
+  // thumbnailer.GenerateAnimation(&webp_data);
+  // ImgIoUtilWriteFile(output, webp_data.bytes, webp_data.size);
+
   std::cout << "Compressing: hll wrld" << std::endl;
   return 0;
 }
